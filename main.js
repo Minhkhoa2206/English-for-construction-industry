987e0160a6b015b8ec1c329e7d7a04fc1c7bdb08
Minhkhoa2206 <97280663+Minhkhoa2206@users.noreply.github.com>
2023-10-31 13:32:24 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function changeLanguage() {
+    var language = document.getElementById("language").value;
+    if (language === "vi-en") {
+        window.location.href = "index2.html";
+    }
+}
+
+function lookup() {
+    var word = document.getElementById("word").value.toLowerCase(); // Convert the input word to lowercase
+    var output = document.getElementById("output");
+    
+    // Convert each word in the dictionary to lowercase for the comparison
+    for (var key in dictionary) {
+        if (key.toLowerCase() === word) {
+            output.innerHTML = "Pronunciation: /" + dictionary[key].pronunciation + "/ ";
+            output.innerHTML += "<button id='speak' class='speak-button'>Đọc</button><br>";
+            output.innerHTML += "Meaning Vietnamese: " + dictionary[key].meaning;
+
+            var speakButton = document.getElementById("speak");
+            speakButton.onclick = function() {
+                // Create a new speech synthesis utterance for the English word
+                var utterance = new SpeechSynthesisUtterance(word);
+
+                // Set the language to English
+                utterance.lang = 'en-US';
+
+                // Get the list of voices available
+                var voices = window.speechSynthesis.getVoices();
+
+                // Filter for female voices and set the voice for the utterance
+                var femaleVoices = voices.filter(function(voice) { return voice.gender === 'female'; });
+                if (femaleVoices.length > 0) {
+                    utterance.voice = femaleVoices[0];
+                }
+
+                // Speak the utterance
+                window.speechSynthesis.speak(utterance);
+            }
+            return;
+        }
+    }
+
+    output.innerHTML = "Word not found in dictionary.";
+}
---
+++ main2.js
@@ -0,0 +1,48 @@
+// Khai báo biến ngôn ngữ hiện tại
+var currentLanguage = "vi-en";
+
+// Hàm thay đổi ngôn ngữ
+function changeLanguage() {
+    currentLanguage = document.getElementById("language").value;
+    if (currentLanguage === "en-vi") {
+        window.location.href = "index.html";
+    }
+}
+
+// Hàm viết hoa chữ cái đầu tiên
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Hàm tra cứu từ
+function lookup() {
+    var word = document.getElementById("word").value; // Lấy từ nhập vào
+    var lowerCaseWord = word.toLowerCase(); // Chuyển từ nhập vào thành chữ thường
+    var capitalizedWord = capitalizeFirstLetter(lowerCaseWord); // Chuyển chữ cái đầu tiên thành chữ hoa
+    var output = document.getElementById("output");
+    
+    if (dictionary[word]) {
+        output.innerHTML = "Phiên âm: /" + dictionary[word].pronunciation + "/ <button onclick='speak(\"" + dictionary[word].meaning + "\")'>Đọc</button><br>" +
+                           "Từ Vựng: " + dictionary[word].meaning;
+    } else if (dictionary[lowerCaseWord]) {
+        output.innerHTML = "Phiên âm: /" + dictionary[lowerCaseWord].pronunciation + "/ <button onclick='speak(\"" + dictionary[lowerCaseWord].meaning + "\")'>Đọc</button><br>" +
+                           "Từ Vựng: " + dictionary[lowerCaseWord].meaning;
+    } else if (dictionary[capitalizedWord]) {
+        output.innerHTML = "Phiên âm: /" + dictionary[capitalizedWord].pronunciation + "/ <button onclick='speak(\"" + dictionary[capitalizedWord].meaning + "\")'>Đọc</button><br>" +
+                           "Từ Vựng: " + dictionary[capitalizedWord].meaning;
+    } else {
+        output.innerHTML = "Không tìm thấy từ trong từ điển.";
+    }
+}
+
+// Hàm phát âm từ
+function speak(word) {
+    // Tạo một thể hiện mới của SpeechSynthesisUtterance
+    var utterance = new SpeechSynthesisUtterance(word);
+    
+    // Đặt ngôn ngữ
+    utterance.lang = 'en-US';
+    
+    // Phát âm từ
+    window.speechSynthesis.speak(utterance);
+}
