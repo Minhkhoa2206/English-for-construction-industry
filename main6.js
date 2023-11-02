10f88c05a08042a3de4871fd3a085bd6e12cca7c
Minhkhoa2206 <97280663+Minhkhoa2206@users.noreply.github.com>
2023-11-02 16:13:04 +0700

    Update main6.js

--- main6.js
+++ main6.js
@@ -10,21 +10,81 @@ window.onload = function() {
     }
     
     var deviceId = localStorage.getItem('device_id');
+    var isNewSession = getCookie('isNewSession');
     
     if (!deviceId) {
-        deviceId = 'xxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16 | 0,
-                v = c === 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-        
+        deviceId = generateId();
         localStorage.setItem('device_id', deviceId);
     }
     
-    var text = "Có người vừa truy cập trang web của bạn từ một thiết bị " + device + "! ID thiết bị: " + deviceId;
+    var browser = getBrowser();
+    
+    if (!isNewSession || isNewSession === 'false') {
+        setCookie('isNewSession', 'true', 0); // Cookie will expire when the browser is closed
+        var text = "Thiết bị mới có ID " + deviceId + " vừa truy cập website từ một thiết bị " + device + " sử dụng trình duyệt " + browser + "!";
+    } else {
+        text = "Thiết bị cũ ID " + deviceId + " vừa truy cập website từ một thiết bị " + device + " sử dụng trình duyệt " + browser + "!";
+    }
+    
     var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${text}`;
     
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
     xhr.send();
+}
+
+function generateId() {
+    var id = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    
+    while (id.length < 5 || !/\d/.test(id) || !/[a-zA-Z]/.test(id)) {
+        id += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    
+    return id;
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    
+    for(var i=0; i < ca.length; i++) {
+        var c = ca[i];
+        
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    
+    return null;
+}
+
+function getBrowser() {
+  var userAgent = navigator.userAgent;
+
+  if (userAgent.indexOf("Chrome") > -1) {
+      return 'Google Chrome';
+  } else if (userAgent.indexOf("Safari") > -1) {
+      return 'Apple Safari';
+  } else if (userAgent.indexOf("Opera") > -1) {
+      return 'Opera';
+  } else if (userAgent.indexOf("Firefox") > -1) {
+      return 'Mozilla Firefox';
+  } else if (userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident") > -1) {
+      return 'Microsoft Internet Explorer';
+  } else {
+      return 'Unknown';
+  }
 }
